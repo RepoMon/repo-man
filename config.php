ab438bae420ad0273ba87090b67d80a2a879cd77
Tim Rodger <tim.rodger@gmail.com>
2015-07-14 22:24:15 +0100

    Removes specific repos

--- config.php
+++ config.php
@@ -9,9 +9,6 @@ class Config
     public function getRepositories()
     {
         return [
-            'https://github.com/SCEE/UnifiedPortal',
-            'https://github.com/SCEE/AccountManagementService',
-            'https://github.com/SCEE/MessageBusHttpDispatcher',
         ];
     }
 }
