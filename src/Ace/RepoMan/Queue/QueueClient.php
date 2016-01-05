3de05f5fab34729bfc6641c700ea172874bfabea
Tim Rodger <tim.rodger@gmail.com>
2016-01-05 21:37:31 +0000

    Updates  QueueClient

--- src/Ace/RepoMan/Queue/QueueClient.php
+++ src/Ace/RepoMan/Queue/QueueClient.php
@@ -1,8 +1,9 @@
-<?php namespace Ace\RepoMan\Queue;
+<?php namespace Ace\Scheduler\Queue;
 
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
-use PhpAmqpLib\Channel\AMQPChannel;;
+use PhpAmqpLib\Channel\AMQPChannel;
+use Exception;
 
 /**
  * @author timrodger
@@ -35,6 +36,10 @@ class QueueClient
      */
     private $channel;
 
+    /**
+     * @var array
+     */
+    private $event_handlers = [];
 
     /**
      * @param $host
@@ -88,11 +93,37 @@ class QueueClient
     }
 
     /**
+     * Add a handler function for an event
+     *
+     * @param $event
      * @param callable $callback
      */
-    public function consume(callable $callback)
+    public function addEventHandler($event, callable $callback)
     {
-        //printf(" %s host %s port %s channel %s\n", __METHOD__, $this->host, $this->port, $this->channel_name);
+        $this->event_handlers[$event] = $callback;
+    }
+
+    /**
+     * Sit in loop waiting for incoming messages
+     * Call event handlers to consume events
+     */
+    public function consume()
+    {
+        $handlers = $this->event_handlers;
+
+        $callback = function($message) use ($handlers) {
+
+            $event = json_decode($message->body, true);
+
+            if (array_key_exists($event['name'], $handlers)){
+                try {
+                    $handlers[$event['name']]($event);
+                } catch (Exception $ex) {
+                    print "Error handling " . $event['name'] . "\n\n" . $ex->getMessage();
+                }
+            }
+        };
+
         $this->connect();
 
         list($queue_name, ,) = $this->channel->queue_declare("", false, false, true, false);
--- src/consume.php
+++ src/consume.php
@@ -21,72 +21,67 @@ $app['logger']->notice(sprintf("rabbit host: %s port: %s channel: %s\n",
     )
 );
 
-$callback = function($msg) use ($app) {
+$updateHandler = function($event) use ($app) {
+    // use $event['data']['full_name'] to get owner and url from this service
 
-    $app['logger']->notice(" Received " . $msg->body);
+    $repository = $app['store']->get($event['data']['full_name']);
 
-    $event = json_decode($msg->body, true);
+    $token = $app['token-service']->getToken($repository['owner']);
 
-    if ($event['name'] === 'repo-mon.update.scheduled') {
-        // update this repo
+    // update the repository, locally using the token
+    // pass the dependency manager and lang to the factory
+    $command = $app['command_factory']->create(
+        'dependencies/update/current',
+        $repository['url'],
+        $token
+    );
 
-        // use $event['data']['full_name'] to get owner and url from this service
-
-        $repository = $app['store']->get($event['data']['full_name']);
-
-        $token = $app['token-service']->getToken($repository['owner']);
-
-        // update the repository, locally using the token
-        // pass the dependency manager and lang to the factory
-        $command = $app['command_factory']->create(
-            'dependencies/update/current',
-            $repository['url'],
-            $token
-        );
-
-        $command->execute();
-
-    } else if ($event['name'] === 'repo-mon.repository.added') {
-
-        $data = $event['data'];
-
-        $active = 0;
-
-        // should not overwrite existing data?
-        $result = $app['store']->add(
-            $data['url'],
-            $data['full_name'],
-            $data['owner'],
-            $data['description'],
-            $data['language'],
-            $data['dependency_manager'],
-            $data['timezone'],
-            $active
-        );
-        echo " Result of add is '$result'\n";
-
-    } else if ($event['name'] === 'repo-mon.repository.removed') {
-
-        $result = $app['store']->delete(
-            $event['data']['full_name']
-        );
-        echo " Result of delete is '$result'\n";
-
-    } else if ($event['name'] === 'repo-mon.repository.activated') {
-
-        $result = $app['store']->activate(
-            $event['data']['full_name']
-        );
-        echo " Result of activate is '$result'\n";
-
-    } else if ($event['name'] === 'repo-mon.repository.deactivated') {
-
-        $result = $app['store']->deactivate(
-            $event['data']['full_name']
-        );
-        echo " Result of deactivate is '$result'\n";
-
-    }
+    $command->execute();
 };
 
-$app['queue-client']->consume($callback);
+$addedHandler = function($event) use ($app) {
+
+    $data = $event['data'];
+
+    // should not overwrite existing data?
+    $result = $app['store']->add(
+        $data['url'],
+        $data['full_name'],
+        $data['owner'],
+        $data['description'],
+        $data['language'],
+        $data['dependency_manager'],
+        $data['timezone'],
+        $active  = 0
+    );
+    echo " Result of add is '$result'\n";
+};
+
+$removedHandler = function ($event) use ($app) {
+    $result = $app['store']->delete(
+        $event['data']['full_name']
+    );
+    echo " Result of deactivate is '$result'\n";
+};
+
+$activatedHandler = function ($event) use ($app) {
+    $result = $app['store']->activate(
+        $event['data']['full_name']
+    );
+    echo " Result of deactivate is '$result'\n";
+};
+
+$deactivatedHandler = function ($event) use ($app) {
+    $result = $app['store']->deactivate(
+        $event['data']['full_name']
+    );
+    echo " Result of deactivate is '$result'\n";
+};
+
+$app['queue-client']->addEventHandler('repo-mon.repository.added', $addedHandler);
+$app['queue-client']->addEventHandler('repo-mon.repository.activated', $activatedHandler);
+$app['queue-client']->addEventHandler('repo-mon.repository.deactivated', $deactivatedHandler);
+$app['queue-client']->addEventHandler('repo-mon.repository.removed', $removedHandler);
+$app['queue-client']->addEventHandler('repo-mon.update.scheduled', $updateHandler);
+
+$app['queue-client']->consume();
